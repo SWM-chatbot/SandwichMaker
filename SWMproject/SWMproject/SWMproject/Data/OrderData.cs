798213b90ec3202ce5708a9233bd41472b8ee2fe
ji-water <bjisoo72@gmail.com>
2020-07-10 16:27:32 +0900

    add checking topping text

--- SWMproject/SWMproject/SWMproject/Data/OrderData.cs
+++ SWMproject/SWMproject/SWMproject/Data/OrderData.cs
@@ -14,18 +14,20 @@ namespace SWMproject.Data
         //3. 빵 선택
         public string Bread { get; set; }
         //4. 치즈 선택
-        public string Cheese { get; set; }
+        public List<string> Cheese { get; set; } = new List<string>() ;
         //5. 데우기
         public bool Warmup { get; set; }
-        //6. 빼는 야채
-        public string[] Vege { get; set; } = { "토마토", "올리브","양상추","양파","파프리카","오이","피망","피클","할라피뇨" };
+        //추가토핑
+        public List<string> Topping { get; set; } = new List<string>();
+        //6. 야채
+        public List<string> Vege { get; set; } = new List<string>();
         //7. 소스 선택
-        public string Sauce { get; set; }
+        public List<string> Sauce { get; set; } = new List<string>();
         //8. 세트 선택
         public string SetMenu { get; set; }
         public string SetDrink { get; set; }
         //단품
-        public string[] AddiOrder { get; set; }
+        public List<string> AddiOrder { get; set; } = new List<string>();
         //9. 요구사항
         public string Requirement { get; set; }
     }
